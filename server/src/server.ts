daaa61f27a8ac93c7390474a06b79ab4e2173eb9
Alan Silva <alan@kea.cloud>
2025-07-12 20:59:56 +0100

    feat: rooms crud

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,12 +6,15 @@ import {
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 import { env } from './env.ts'
+import { createQuestionRoute } from './http/routes/create-question.ts'
+import { createRoomRoute } from './http/routes/create-room.ts'
+import { getRoomQuestions } from './http/routes/get-room-questions.ts'
 import { getRoomsRoute } from './http/routes/get-rooms.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
-  origin: 'http://localhost:5173',
+  origin: '*',
 })
 
 app.setSerializerCompiler(serializerCompiler)
@@ -22,5 +25,8 @@ app.get('/health', () => {
 })
 
 app.register(getRoomsRoute)
+app.register(createRoomRoute)
+app.register(getRoomQuestions)
+app.register(createQuestionRoute)
 
 app.listen({ port: env.PORT })
